3fa8de341b1b3b785a79a4317e21480b12dbdb9a
sanketvarpe <varpesanket2000@gmail.com>
2020-11-30 22:20:19 +0530

    server side modified

---
+++ server/src/main/java/com/miniproject/sdl/genrateQrCode.java
@@ -0,0 +1,39 @@
+package com.miniproject.sdl;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class genrateQrCode {
+	
+	@SuppressWarnings("deprecation")
+	public static void createQR(String data, String path,
+            String charset, Map hashMap,
+            int height, int width)
+            throws WriterException, IOException
+    	{
+
+		BitMatrix matrix = new MultiFormatWriter().encode(
+		new String(data.getBytes(charset), charset),
+		BarcodeFormat.QR_CODE, width, height);
+		
+		MatrixToImageWriter.writeToFile(
+		matrix,
+		path.substring(path.lastIndexOf('.') + 1),
+		new File(path));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
